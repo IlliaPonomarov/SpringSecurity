d7e2924c07741f556f76aa6bb85d9f5c49494c36
Illia <chippaxyucac@gmail.com>
2022-07-04 13:02:50 +0200

    Registartion page

---
+++ src/main/java/ua/ponomarov/illia/springsecuritycourse/controllers/AuthController.java
@@ -0,0 +1,19 @@
+package ua.ponomarov.illia.springsecuritycourse.controllers;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "auth/login";
+    }
+
+}
